f5d9455cc01ea2b9001ac18e0f7150649f56edf9
Alexander <81513713+HeyageroT@users.noreply.github.com>
2023-01-08 20:56:21 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,8 @@ sudo dnf -y install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-non
 sudo dnf -y install fedora-workstation-repositories bash-completion
 sudo dnf config-manager --set-enabled google-chrome
 sudo dnf update
-sudo dnf -y install plasma-desktop plasma-nm bluedevil dolphin konsole kate ark spectacle kcalc sddm sddm-kcm sddm-breeze kde-print-manager kscreen kde-partitionmanager plasma-systemmonitor google-chrome-stable
+sudo dnf -y install plasma-desktop plasma-nm bluedevil dolphin konsole kwrite ark spectacle kcalc plasma-discover kde-print-manager kscreen kde-partitionmanager plasma-systemmonitor  
+sudo dnf -y install sddm sddm-kcm sddm-breeze plymouth-kcm google-chrome-stable pciutils plasma-disks kinfocenter kde-connect
 sudo sed -i 15i\ 'InputMethod=' /etc/sddm.conf
 sudo systemctl enable sddm
 sudo systemctl enable bluetooth
