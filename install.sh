e4daa356aa7d5bc22e2d1efa573ce938bb8f8922
Alexander <81513713+HeyageroT@users.noreply.github.com>
2023-01-12 02:13:58 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo dnf -y install fedora-workstation-repositories bash-completion
 sudo dnf config-manager --set-enabled google-chrome
 sudo dnf update
 sudo dnf -y install plasma-desktop plasma-nm bluedevil dolphin konsole kwrite ark spectacle kcalc plasma-discover kde-print-manager kscreen kde-partitionmanager plasma-systemmonitor  
-sudo dnf -y install sddm sddm-kcm sddm-breeze plymouth-kcm plymouth-theme-breeze google-chrome-stable pciutils plasma-disks kinfocenter kde-connect
+sudo dnf -y install sddm sddm-kcm sddm-breeze plymouth-kcm plymouth-theme-breeze google-chrome-stable pciutils plasma-disks kinfocenter kde-connect kdeplasma-addons
 sudo sed -i 15i\ 'InputMethod=' /etc/sddm.conf
 sudo sed -i '1 s/5/0/' /etc/default/grub
 sudo grub2-mkconfig -o /etc/grub2.cfg
